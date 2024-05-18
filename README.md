a9e17275b16c044a8ad9247103e4de38126e5c07
circleorange <piotrbielski3@gmail.com>
2024-05-18 12:41:24 +0100

    basic array example

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # java-dstruct
+
+## Purpose
+- Deep dive into Java data structures
--- app/src/main/java/dstruct/Array.java
+++ app/src/main/java/dstruct/Array.java
@@ -1,3 +1,19 @@
+package main.java.dstruct;
+
 public class Array {
 
+    private int [] array;
+    
+    public Array() {
+
+        array = new int[3];
+
+        array[0] = 20;
+        array[1] = 15;
+        array[2] = 10;
+    }
+
+    public int[] getArray() {
+        return array;
+    }
 }
